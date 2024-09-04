9ac7811870ad6ec51a76817d599e5834e7880854
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-09-04 20:00:27 +0330

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,47 @@
-// 3-17-Adding a Markdown Editor
+// 3-18-Handling Form Submission
 "use client"
 
 import { Button, TextField } from '@radix-ui/themes'
 import React from 'react'
-//1- we import the simple MD component and a corresponding CSS file.
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+  title: string,
+  description: string
+}
 
 
 const NewIssuePage = () => {
+  const router =useRouter()
+  //12-Now finally, we want to redirect the user with the issues page.
+  //To do that, we have to use the router hook in Next.js.
+
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+//11-Instead of logging this data object on the console, we want to
+//send it to our API and save in database.To do that, we're going to 
+//use axios.We can also use the fetch function in browsers.let's install
+//and import it.
   return (
-    <div className='max-w-xl space-y-3'>
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async(data) => { 
+      await axios.post("/api/issues", data) 
+      router.push("/issues")
+      })}>
       <TextField.Root>
-        <TextField.Input placeholder='title' />
+        <TextField.Input placeholder='title' {...register('title')} />
       </TextField.Root>
-      {/* 2-then we replace <TeaxtArea> with <SimpleMDE> */}
-      <SimpleMDE placeholder='Description'/>
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
-//3-Now we have this markdown editor.(fig 17-2).We can also customize it.
+
 
 export default NewIssuePage
