7a778cbaa639ea961591f70955cd22650d300743
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-09-16 11:53:53 +0330

    Add additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
-//4-29-Adding Marhdown Preview
+//4-31-Additional Loading Skeletons
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 
 import ReactMarkdown from "react-markdown"
+import delay from 'delay'
 
 interface Props {
     params: { id: string }
@@ -19,6 +20,10 @@ const IssueDetailPage = async ({ params }: Props) => {
     if (!issue)
         notFound()
 
+    //6-Now, to see this in action, here we apply a delay.
+    //Go to issues/new/loading.tsx
+    await delay (2000)
+
     return (
         <div>
             <Heading>{issue.title}</Heading>
@@ -26,8 +31,6 @@ const IssueDetailPage = async ({ params }: Props) => {
                 <IssueStatusBadge status={issue.status} />
                 <Text>{issue.creatdAt.toDateString()}</Text>
             </Flex>
-         {/* 5-Third we have to add the "prose class" to the containing 
-         element.(here the containing element is Card) */}
             <Card className='prose' mt="4"><ReactMarkdown>{issue.description}</ReactMarkdown></Card>
         </div>
     )
