639e05af7eb949059aa8d926df2cbbaffc90ee36
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-09-24 17:45:59 +0330

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,4 +1,4 @@
-//5-36-Building the Edit Issue Page
+//5-38-Updating Issues
 "use client"
 
 import { ErrorMessage, Spinner } from '@/app/components';
@@ -14,9 +14,6 @@ import { z } from 'zod';
 import "easymde/dist/easymde.min.css";
 import { Issue } from '@prisma/client';
 
-//2-Beause we're going to use "this component" only in the "issue section" 
-//of our website.So here in the "issues folder" we create a "components 
-//folder" and add our "IssueForm component" .
 
 
 const SimpleMDE = dynamic(
@@ -24,12 +21,9 @@ const SimpleMDE = dynamic(
     { ssr: false }
 )
 
-//3-We changed the name of our type because It was the same as our 
-//"component's name" here. 
 type IssueFormData = z.infer<typeof IssuesSchema>
 
-//9-we should make this issue type "optional" because it's only needed on
-//the edit page.
+
 interface Props {
     issue?: Issue
 }
@@ -50,7 +44,12 @@ const IssueForm = ({ issue }: Props) => {
     const onSubmit = handleSubmit(async (data) => {
         try {
             setIsSubmitting(true)
-            await axios.post("/api/issues", data)
+            //1-Here If we have issue, we sent a PATCH request for editing
+            //data,otherwise we send a POST request for creating a data.
+            if (issue)
+                await axios.patch("/api/issues/" + issue.id, data)
+            else
+                await axios.post("/api/issues", data)
             router.push("/issues")
         } catch (error) {
             setIsSubmitting(false)
@@ -68,15 +67,12 @@ const IssueForm = ({ issue }: Props) => {
             </Callout.Root>}
             <form className=' space-y-3' onSubmit={onSubmit}>
                 <TextField.Root>
-                    {/* 10-we set defaultValue to our given issue title */}
                     <TextField.Input defaultValue={issue?.title} placeholder='title' {...register('title')} />
                 </TextField.Root>
                 <ErrorMessage>{errors.title?.message}</ErrorMessage>
                 <Controller
                     name='description'
                     control={control}
-                    // 10-we set defaultValue to our given issue 
-                    //description  
                     defaultValue={issue?.description}
                     render={({ field }) => {
                         const { ref, ...rest } = field;
@@ -85,21 +81,22 @@ const IssueForm = ({ issue }: Props) => {
 
                 />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button disabled={isSubmitting}>Submit New Issue {isSubmitting
-                    && <Spinner />}</Button>
+                {/*2-we should also set the label of the button 
+                dynamically.  */}
+                <Button disabled={isSubmitting}>
+                    {issue ? "Update Issue" : "Submit New Issue"}{" "}
+                    {isSubmitting && <Spinner />}</Button>
             </form>
         </div>
     )
 }
 
+//3-Here after we update an issue,if we go to edit page for that issue
+//we don't see the updated issue that's because of a caching issue.We
+//will solve it in the next lesson.For now just refresh the page and 
+//you'll see the updated issue.
+
+
 export default IssueForm
 
-//4-we can optionally rename "the component folder" and prefix it with an
-//underscore ==>_components
-//And with this, we can opt this folder out of "the routing system".So 
-//it's not going to be part of our routing system, even if you put a page
-//file here.This is a good technique for separating "implementation 
-//details" from our "routing folders".(fig 36-1)
-
-//Go to issues/new/page copy 18.tsx
 
