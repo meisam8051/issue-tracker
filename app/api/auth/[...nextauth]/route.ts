759817c7fe3da045967bffee9e7ff8cddbb006b9
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-10-05 17:16:27 +0330

    Configure Google Provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,23 @@
-// 7-48-Setting Up NextAuth
+// 7-49-Configuring Google Provider
 
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+//2-
+import GoogleProvider from "next-auth/providers/google";
 
-//1-we copied this from the documentaion of Nextauth(fig 48-1).
-//To set up NixAuth,In this file, we create a NextAuth handler 
-//and export it as a get and post function.
 const handler = NextAuth({
-  providers:[]
-})
+  providers: [
+    //3-here we have an error saying type, string, or undefined is not 
+    //assignable to type, string.So the TypeScript compiler doesn't know
+    //that we have this variable in our ENV file.So here we have to add 
+    //an exclamation mark at the end to sell the TypeScript compiler 
+    //that yes, we do have a value for this environment variable.
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
 
-//Go to app/.env copy 2 
+
