66816fc24782fa459e367e969443e784fe5c1d9a
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-10-25 13:53:09 +0330

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,24 +1,44 @@
-//7-57-Securing the Application
+//7-63-Implementing the API
 
 import authOption from "@/app/auth/authOption";
-import { IssuesSchema } from "@/app/validationSchemas";
+import { PatchIssuesSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import delay from "delay";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
+//1-we want to extend this PATCH function to update the assignedtoUserid
+//field.
+//Go to app/validationSchemas.ts
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  //11-
+  //6-For testing we should also comment out this two line for preventing 
+  //unauthorized error. 
   const session = await getServerSession(authOption);
   if (!session) return NextResponse.json({}, { status: 401 });
-  //------------
+
   const body = await request.json();
-  const validation = IssuesSchema.safeParse(body);
+  //3-we use our new flexible schema for validation using Zod.
+  const validation = PatchIssuesSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  //4-if the assignedToUserId have a usedId value, in the body of the 
+  //request we want to make sure that that is a valid user.
+  const { title, description, assignedToUserId } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+    }
+  }
+
+  //Here we validate the issue.We want to make sure that the client is 
+  //updating a valid issue.
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(params.id),
@@ -30,22 +50,27 @@ export async function PATCH(
     where: {
       id: issue.id,
     },
+    //5-Now with this implementation if any of the following properties 
+    //are not set if they're undefined they're not going to be updated.
     data: {
-      title: body.title,
-      description: body.description,
+      //here we use destructuring of body to simplify our data object.
+      title,
+      description,
+      assignedToUserId,
     },
   });
   return NextResponse.json(updateIssue);
 }
 
+//6-For testing our implementation,we install postamn extension of vscode
+//and sign in to it.(fig 63-2,63-3,63-4,63-5,63-6,63-7) 
+
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  //12-
   const session = await getServerSession(authOption);
   if (!session) return NextResponse.json({}, { status: 401 });
-  //Thenwe can test it with postman.
 
   const issue = await prisma.issue.findUnique({
     where: {
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,15 +1,36 @@
-// 5-37-Building an API
+//7-63-Implementing the API
 
 import { z } from "zod";
 
-//3-We called our schema, "createIssueSchema", but this name no longer 
-//applies here.So let's rename it to something more general 
-//==>IssuesSchema
-//Then we should change the name of it for all place we import it to use.
-//Go back to api/issues/[id]/route.ts
+export const IssuesSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required.").max(65535),
+});
 
-export const IssuesSchema = z.object({  
-    title: z.string().min(1,"Title is required.").max(255),
-    description: z.string().min(1,"Description is required.")
-})
+//2-We want our patch endpoint to be more flexible.Perhaps we want to
+//update the title and description that is used in our edit issue page or
+//somewhere else using our select component we want to update assigned to
+//user ID and in the future maybe we want to update the status of an issue.
 
+//To To do that we have to create a separate schema and declare all these
+//properties as optional.
+export const PatchIssuesSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535) //The type of the description column is Text (fig 63-1).In
+    //text columns we can store a maximum of 65,535 characters.
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")//If we provide assignedToUserId 
+    //we should have at least one character.
+    .max(255)
+    .optional()
+    .nullable(),//we want to make this nullable so we can explicitly 
+    //provide null here to unassign an issue.
+
+});
+
+//Go back to api/issues/[id]/route copy 5.tsx
