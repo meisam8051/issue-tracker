4242acaa13201b9d60df9241764333ebc5eff3eb
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-10-28 20:44:55 +0330

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,61 +1,44 @@
-//8-65-Showing Toast Notifications 
+//8-66-Refactoring the Assignee Select Component
 
 "use client"
 
+import { Skeleton } from "@/app/components"
 import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import React from 'react'
-import { Skeleton } from "@/app/components"
-//1-Import the toast object and the Toaster component.
+
 import toast, { Toaster } from "react-hot-toast"
 
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 
-
-    const { data: users, error, isLoading } = useQuery<User[]>({
-        queryKey: ["users"],
-        queryFn: () => axios.get("/api/users").then(res => res.data),
-        staleTime: 60 * 1000,
-        retry: 3
-    })
+//2-
+    const { data: users, error, isLoading } = useUser()
 
 
     if (isLoading) return <Skeleton />
     if (error) return null
 
+    //1-
+    const assignIssue = (userId: string) => {
+        axios.patch("/api/issues/" + issue.id, {
+            assignedToUserId: userId || null
+        })
+            .catch(
+                () => {
+                    toast.error("Changes could not be saved.")
+                })
+
+    }
 
 
     return (
         <>
             <Select.Root
                 defaultValue={issue.assignedToUserId || ""}
-                onValueChange={(userId) => {
-                    //3-to handle errors, here we have to await this patch 
-                    //call so we can wrap it in a Try-Catch block.Or instead
-                    //we can use catch method to get an error.
-                    axios.patch("/api/issues/" + issue.id, {
-                        assignedToUserId: userId || null
-                    })
-                        .catch(
-                            //4-we pass a callback function to it
-                            () => { 
-                            //in this function we use the Toast 
-                            //object that we imported on the top to show 
-                            //an error.So here we call the error method 
-                            //and provide a message.
-                            toast.error("Changes could not be saved.") 
-                        })
-                        //5-To simulate it we change the url of patch
-                        //method.(fig 65-1)
-                        //6-you have full control over customizing the 
-                        //toast notification in terms of the duration 
-                        //and look and feel.Go to 
-                        //https://react-hot-toast.com/
-
-                }}>
+                // 1-
+                onValueChange={assignIssue}>
                 <Select.Trigger placeholder='Assign...' />
                 <Select.Content>
                     <Select.Group>
@@ -68,11 +51,43 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
                     </Select.Group>
                 </Select.Content>
             </Select.Root>
-            {/* 2-somewhere in this component we have to add this Toaster 
-        component as a container for showing a Toast notification. */}
             <Toaster />
         </>
     )
+}
+
+//2-because this is the only place where we need this logic for fetching
+// a list of users and caching them.So there is not much value in taking 
+//this logic outside of this component.
+//We can still take this piece of logic and put it in a custom hook 
+//within this module,this will make this code slightly cleaner.
+const useUser = () => {
+    return useQuery<User[]>({
+        queryKey: ["users"],
+        //4-we can move this axios call inside a separate function like 
+        //get users.And put it in a single file where we have
+        //all our API calls.But we don't have that requirement at this 
+        //point.it's always better to fix today's problems as opposed to 
+        //problems in the future.That might never happen.
+
+        //5-what if our endpoints change?With this approach, we have 
+        //multiple places where we have a call to /api/users.So wouldn't 
+        //that be nicer to put this inside a separate function?So we have
+        //a single place where we specify our endpoints.
+        //But your endpoints shouldn't change to start with.So this end 
+        //point is essentially a contract that our application is 
+        //exposing to the outside world.
+
+        queryFn: () => axios.get("/api/users").then(res => res.data),
+        staleTime: 60 * 1000,//3-the 60 seconds is too short.Because We're 
+        //assuming this application is an internal application for an 
+        //organization and we're not going to get a lot of new users in 
+        //a short period of time.So it's safe to use a longer value here,
+        //like 10 minutes or an hour or even one day.It depends on our 
+        //application and its requirements.
+
+        retry: 3
+    })
 }
 
 
