d0ae7db9c16dbdf733f6b6875aae2250df5bebf3
Meisam garshasbi <eng.meisam8051@gmail.com>
2024-11-04 12:16:42 +0330

    Paginate issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,4 +1,4 @@
-//9-70-Sorting Issues
+//9-75-PaginatingIssues
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
 import React from 'react'
@@ -8,11 +8,15 @@ import IssueAction from './IssueActions'
 import { Issue, Status } from '@prisma/client'
 import NextLink from "next/link"
 import { ArrowUpIcon } from '@radix-ui/react-icons'
+import Pagination from '@/app/components/Pagination'
 
 interface Props {
   searchParams: {
     status: Status,
-    orderBy: keyof Issue
+    orderBy: keyof Issue,
+    //1-we should also get page query parameter which is for showing 
+    //that which page we are.
+    page: string
   }
 }
 
@@ -36,22 +40,37 @@ const IssuePage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status) ?
     searchParams.status : undefined
 
-    //4-For preventing to set invalid value to orderBy query parameter 
-    //manually,we validate our searchParams.
-    const validateOrderBy =columns.map(column=>column.value)
+  const validateOrderByValues = columns.map(column => column.value)
 
-    //3-So, to solve this, we have to properly create this object before 
-    //passing it to Prisma.The same way we computed the status.
-  const orderBy =validateOrderBy.includes(searchParams.orderBy) ?
+  const orderBy = validateOrderByValues.includes(searchParams.orderBy) ?
     { [searchParams.orderBy]: "asc" } : undefined
 
+    //forcleaner code we extract this repetitive code.
+  const where = { status }
+
+//2-as part of fetching the issues, we need to fetch the issues for
+//a given page.
+  const page = parseInt(searchParams.page) || 1 //if we cannot parse this 
+  //to a valid number or if the page parameter is not provided,we give 
+  //this a default value of 1.
+  const pageSize = 10//In the future, you can extend this application and 
+  //add a drop-down list for selecting the page size.
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status
-    },
-    orderBy: orderBy
+    where: where,
+    orderBy: orderBy,
+    //3-while fetching issues, here we should add two extra properties.
+    skip: (page - 1) * pageSize,//that is the number of records we should
+    //skip.For example if the page = 2(we are in page 2),10 records of
+    //our issues are skipped.
+    take: pageSize //this is the number of records we want to fetch for
+    //showing our specified page.
+
   })
 
+  //4-we also need to know the total number of issues so we can pass it 
+  //to our pagination component.
+  const issueCount =await prisma.issue.count({ where: where })
 
 
   return (
@@ -100,6 +119,8 @@ const IssuePage = async ({ searchParams }: Props) => {
           )}
         </Table.Body>
       </Table.Root>
+      {/* 5- */}
+      <Pagination currentPage={page} pageSize={pageSize} itemCount={issueCount} />
     </div>
   )
 }
